ce477d0e34914807204c7ba279f95957578e9d13
liyu <yszli@thoughtworks.com>
2021-04-06 21:54:00 +0800

    for loop

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,12 @@
+import random
 def main():
-  print('You rolled a die')
+  dice_roll = 2
+  dice_sum = 0
+  for i in range(0,dice_roll):
+    roll = random.randint(0,6)
+    dice_sum += roll
+    print('You rolled a', roll)
+    print('You have rolled a total of ', dice_sum)
 
 if __name__== "__main__":
   main()
